460dc6dbe045a9c7f4d4e2c8ae2c813aefd44f55
CHOI <tlacjd665@gmail.com>
2022-07-14 01:53:34 +0900

    배포테스트2

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
-const router = jsonServer.router('/movies.json');
+const router = jsonServer.router('./movies.json');
 const middlewares = jsonServer.defaults({
-  static: path.resolve(__dirname + '/'),
+  static: './dist',
 });
 
 const port = process.env.PORT || 8000;
