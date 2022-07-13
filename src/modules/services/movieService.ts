42e8b7f91abdd54f31de00b817ea865da247cd1b
CHOI <tlacjd665@gmail.com>
2022-07-14 02:20:20 +0900

    배포테스트5

--- src/modules/services/movieService.ts
+++ src/modules/services/movieService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const movieService = axios.create({
   //baseURL: 'http://localhost:8000/movies',
-  baseURL: `https://sheepplay.herokuapp.com:8000/movies`,
+  baseURL: `https://sheepplay.herokuapp.com/movies`,
 });
 
 export const movieRequest = new HttpRequest(movieService);
